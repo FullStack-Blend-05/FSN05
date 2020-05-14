9c41533bdd50b28dab5140988a32bc342717d3b6
gabriel-brunetti <gabriel.brunetti1@gmail.com>
2020-05-14 20:41:41 -0300

    estrutura Laravel e frontend

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginasController extends Controller
+{
+    // Mostrar a pagina index
+    public function index (){
+        return view('index');
+    }
+
+    // Mostrar a pagina checkout
+    public function checkout(){
+        return view('checkout');
+    }
+    
+    // Mostrar a pagina store
+    public function store(){
+        return view('store');
+    }
+    
+    // Mostrar a show produto
+    public function show($id){
+        return view('product');
+    }
+}
