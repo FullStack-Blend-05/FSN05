6f7f8ee8a6ba1040b2d4dd651c10762124cb4d28
Leonardo Tumadjian <tumadjian@gmail.com>
2020-05-28 22:55:03 -0300

    Adiciona migrations e seed de produtos e categoria

--- database/seeds/ProdutosTableSeeder.php
+++ database/seeds/ProdutosTableSeeder.php
@@ -14,13 +14,15 @@ class ProdutosTableSeeder extends Seeder
         DB::table('produtos')->insert([
             'nome' => 'Macbook', 
             'preco' => 200.00, 
-            'categoria_id' => 1
+            'categoria_id' => 1,
+            'img' => '/img/product01.png'
         ]);
 
         DB::table('produtos')->insert([
             'nome' => 'Tablet Sony', 
             'preco' => 500.00, 
-            'categoria_id' => 2
+            'categoria_id' => 2,
+            'img' => '/img/product04.png'
         ]);
     }
 }
